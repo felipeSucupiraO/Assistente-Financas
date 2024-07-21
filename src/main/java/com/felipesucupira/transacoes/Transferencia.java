94b01691d9afb88496143038750decf6922d0e99
Aust2000 <felipeuberaba10@gmail.com>
2024-07-21 00:15:38 -0300

    feat(Transferencia):criada subclasse de transacao

---
+++ src/main/java/com/felipesucupira/transacoes/Transferencia.java
@@ -0,0 +1,33 @@
+package com.felipesucupira.transacoes;
+
+import com.felipesucupira.Conta;
+
+public class Transferencia extends Transacao{
+    private Conta contaDestino;
+    private Receita transacaoReceita;
+    private Despesa transacaoDespesa;
+    
+    public Transferencia (String nome, float valor, Conta contaAssociada, Conta contaDestino) {
+        super(nome, valor, contaAssociada);
+        this.contaDestino = contaDestino;
+
+        transacaoReceita = new Receita(nome + " receita", valor, contaDestino);
+        transacaoDespesa = new Despesa(nome + " despesa", valor, contaAssociada);
+    }
+
+
+    @Override
+    public void setValor(float valor) {
+        transacaoReceita.setValor(valor);
+        transacaoDespesa.setValor(valor);
+    }
+
+
+    public void mudarContaAssociada(Conta novaConta) {
+        transacaoDespesa.mudarContaAssociada(novaConta);
+    }
+
+    public void mudarContaDestino(Conta novaConta) {
+        transacaoReceita.mudarContaAssociada(novaConta);
+    }
+}
