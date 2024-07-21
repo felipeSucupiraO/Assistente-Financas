13ab579be0a6685b49f7b707e8ff953c93256485
Aust2000 <felipeuberaba10@gmail.com>
2024-07-21 00:14:17 -0300

    feat(Receita):criado override para funcao setValor

--- src/main/java/com/felipesucupira/transacoes/Receita.java
+++ src/main/java/com/felipesucupira/transacoes/Receita.java
@@ -1,14 +1,24 @@
 package com.felipesucupira.transacoes;
 
-import java.time.LocalDate;
-
 import com.felipesucupira.Conta;
 
 // Representa uma transação de ganho
 public class Receita extends Transacao {
-    public Receita(String nome, float valor, Conta contaAssociada) {
+    public Receita (String nome, float valor, Conta contaAssociada) {
         super(nome, valor, contaAssociada);
         getContaAssociada().aumentarSaldo(valor);
+    }
+
+
+    @Override
+    public void setValor(float valor) {
+        float diferenca = valor - getValor();
+        if (diferenca > 0) {
+            getContaAssociada().aumentarSaldo(diferenca);
+        } else if (diferenca < 0) {
+            getContaAssociada().diminuirSaldo(diferenca * -1);
+        }
+        super.setValor(valor);
     }
 
 
