150a0ca4fe9c1bed92d19179e9a762fc80424f84
felipeSucupiraO <felipeuberaba10@gmail.com>
2024-07-26 15:14:51 -0300

    refactor:adicionados divisores nas classes e mudança em nomes de variaveis e funções

--- src/main/java/com/felipesucupira/Conta.java
+++ src/main/java/com/felipesucupira/Conta.java
@@ -1,35 +1,38 @@
 package com.felipesucupira;
 
-// Essa classe representa uma conta onde se tem dinheiro, como uma conta
-// poupança, uma conta corrente ou até a carteira.
+// Representa uma conta onde se tem dinheiro, como uma conta poupança, uma conta
+// corrente ou até a carteira.
 public class Conta {
     private String nome;
     private float saldo;
 
-
+    // -------------------------------------------------------------------------
+    
     public Conta(String nome, float saldoInicial) {
         this.nome = nome;
         saldo = saldoInicial;
     }
-
-
+    
+    // -------------------------------------------------------------------------
+    
     public String getnome() {
         return nome;
     }
-
+    
     public void setnome(String nome) {
         this.nome = nome;
     }
-
+    
     public float getSaldo() {
         return saldo;
     }
     
-
+    // -------------------------------------------------------------------------
+    
     public void aumentarSaldo(float valor) {
         saldo += valor;
     }
-
+    
     public void diminuirSaldo(float valor) {
         saldo -= valor;
     }
--- src/main/java/com/felipesucupira/Usuario.java
+++ src/main/java/com/felipesucupira/Usuario.java
@@ -2,33 +2,35 @@ package com.felipesucupira;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.felipesucupira.mediator.RelacaoTransacoesContasHandler;
-import com.felipesucupira.transacoes.*;;
+import com.felipesucupira.transacoes.*;
 
+// Representa um usuário do programa, com as próprias transações e contas
 public class Usuario {
     private String nome;
     private String senha;
     private List<Conta> listaContas = new ArrayList<Conta>();
     private List<Transacao> listaTransacoes = new ArrayList<Transacao>();
-    private RelacaoTransacoesContasHandler mediator;
+    private RelacaoTransacoesContasHandler mediatorTransacoesContas;
 
-
+    // -------------------------------------------------------------------------
+    
     public Usuario(String nome, String senha) {
         this.nome = nome;
         this.senha = senha;
-        mediator = new RelacaoTransacoesContasHandler(this);
+        mediatorTransacoesContas = new RelacaoTransacoesContasHandler(this);
     }
-
-
+    
+    // -------------------------------------------------------------------------
+    
     public String getNome() {
         return nome;
     }
-
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    
     public String getSenha() {
         return senha;
     }
@@ -37,26 +39,27 @@ public class Usuario {
         this.senha = senha;
     }
     
-    public RelacaoTransacoesContasHandler getMediator() {
-        return mediator;
+    public RelacaoTransacoesContasHandler getMediatorTransacoesContas() {
+        return mediatorTransacoesContas;
     }
-
+    
     public List<Conta> getListaContas() {
         return listaContas;
     }
-
+    
     public List<Transacao> getListaTransacoes() {
         return listaTransacoes;
     }
-
-
+    
+    // -------------------------------------------------------------------------
+    
     public void adicionarConta(Conta contaAdicionada) {
         listaContas.add(contaAdicionada);
     }
         
     public void adicionarTransacao(Transacao transacao) {
-        transacao.setMediator(mediator);
-        mediator.transacaoAdicionada(transacao);
+        transacao.setMediatorTransacoesContas(mediatorTransacoesContas);
+        mediatorTransacoesContas.notifyTransacaoAdicionada(transacao);
         listaTransacoes.add(transacao);
     }
 
@@ -75,14 +78,13 @@ public class Usuario {
         listaContas.remove(conta);
     }
 
-
     public void deletarTransacao(int index) {
         Transacao transacaoDeletada = listaTransacoes.get(index);
         deletarTransacao(transacaoDeletada);
     }
 
     public void deletarTransacao(Transacao transacao) {
-        mediator.transacaoDeletada(transacao);
+        mediatorTransacoesContas.notifyTransacaoDeletada(transacao);
         listaTransacoes.remove(transacao);
     }
 }
