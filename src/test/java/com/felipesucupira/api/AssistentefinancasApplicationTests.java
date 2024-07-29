7d12d94f31a988960e2d4a04a209d4fef84bf2a5
felipeSucupiraO <felipeuberaba10@gmail.com>
2024-07-29 19:46:21 -0300

    feat:mais algumas configurações do spring concluidas

---
+++ src/test/java/com/felipesucupira/api/AssistentefinancasApplicationTests.java
@@ -0,0 +1,13 @@
+	package com.felipesucupira.api;
+
+	import org.junit.jupiter.api.Test;
+	import org.springframework.boot.test.context.SpringBootTest;
+
+	@SpringBootTest
+	class AssistentefinancasApplicationTests {
+
+		@Test
+		void contextLoads() {
+		}
+
+	}
